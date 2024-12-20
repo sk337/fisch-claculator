f7aafbb012b4b1f7c3c5313a7ca80a9c7550869b
sk337 <79583517+sk337@users.noreply.github.com>
2024-12-20 21:04:18 +0000

    add Project description

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -20,7 +20,14 @@ function Index() {
   const [selectedBait, setSelectedBait] = useState<BaitType | undefined>()
   
   return (
-    <div className="p-2">
+    <div className="p-2 flex flex-col gap-4">
+      <span className='text-2xl font-bold'>Fisch Odds Calculator</span>
+      <p>
+        A Work In Progress odds calculator for the game Fisch. <br />
+        My plan for this is to allow it to calculate profit over time for different fish, rod, location, and bait combinations. I also plan show the odds of catching any specified fish for bestiary. <br />
+        I have only gotten some data and for now you can play with some components I have made for the project. <br/>
+        Oh yeah and this is all 100% <i>TypeSafe&trade;</i>. You can contact on discord at <i className='underline'>sk337</i>.
+      </p>
       <SelectRod onChange={setSelectedRod} value={selectedRod} />
       <SelectMutation onChange={setSelectedMutation} value={selectedMutation} />
       <SelectSeason onChange={setSelectedSeason} value={selectedSeason} />
